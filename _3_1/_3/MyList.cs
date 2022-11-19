8ce91e2af5d72307e5f21c39aafa3e7ac1f6538a
Dmitri Kakaulin <d.kakaulin@xlab-game.com>
2022-11-19 03:17:20 +0300

    3_1_3

--- _3_1/_3/MyList.cs
+++ _3_1/_3/MyList.cs
@@ -18,25 +18,49 @@ public class MyList<TItem> : IEnumerable<TItem>
         }
     }    
 
+    public MyList(TItem[] array)
+    {
+        this.m_array = array;
+    }
+    public MyList()
+    {
+    }
+
     public void Add(TItem item)
     {
         Array.Copy(m_array, m_array = new TItem[++Count], Count - 1);
         m_array[Count - 1] = item;
     }
 
-    public void Remove(TItem item)
+    public void Insert(Int32 index, TItem item)
     {
-        for(int i = 0; i < Count; i++)
+            if(index < 0 || index > Count)
+            {
+                Console.WriteLine("Индекс за границами промежутка");
+                return;
+            }
+            TItem[] arrayAssembler = new TItem[++Count];
+            Array.Copy(m_array, arrayAssembler, index);
+            arrayAssembler[index] = item;
+            Array.Copy(m_array, index, arrayAssembler, index + 1, Count - index - 1);
+            m_array = arrayAssembler;
+            return;
+    }
+
+    public void InsertRange(Int32 index, System.Collections.Generic.IEnumerable<TItem> arrayData)
+    {
+        if(index < 0 || index > Count)
         {
-            if(Comparer<TItem>.Default.Compare(m_array[i], item) == 0)
-                {
-                    TItem[] arrayAssembler = new TItem[--Count];
-                    Array.Copy(m_array, arrayAssembler, i);
-                    Array.Copy(m_array, i + 1, arrayAssembler, i, Count - i);
-                    m_array = arrayAssembler;
-                    return;
-                }
+            Console.WriteLine("Индекс за границами промежутка");
+            return;
         }
+        TItem[] DataArray = arrayData.ToArray<TItem>();
+        TItem[] arrayAssembler = new TItem[Count + DataArray.Length];
+        Array.Copy(m_array, arrayAssembler, index);
+        Array.Copy(DataArray, 0, arrayAssembler, index, DataArray.Length);
+        Array.Copy(m_array, index, arrayAssembler, index + DataArray.Length, Count - index);
+        m_array = arrayAssembler;
+        return;
     }
 
     // public void RemoveTestForLoop(TItem item)
@@ -64,13 +88,56 @@ public class MyList<TItem> : IEnumerable<TItem>
     //     }
     // }
 
+    // public void RemoveTestForLoop2(TItem item)
+    // {
+    //     for(int i = 0; i < Count; i++)
+    //     {
+    //         if(Comparer<TItem>.Default.Compare(m_array[i], item) == 0)
+    //             {
+    //                 TItem[] arrayAssembler = new TItem[--Count];
+    //                 for (int j = 0; j < i; j++)
+    //                 {
+    //                     arrayAssembler[j] = m_array[j];
+    //                 }
+    //                 for (int j = i; j < Count; j++)
+    //                 {
+    //                     arrayAssembler[j] = m_array[j - 1];
+    //                 }
+
+                    
+    //                 m_array = arrayAssembler;
+    //                 return;
+    //             }
+    //     }
+    // }
+
+    public void Remove(TItem item)
+    {
+        for(int i = 0; i < Count; i++)
+        {
+            if(Comparer<TItem>.Default.Compare(m_array[i], item) == 0)
+                {
+                    TItem[] arrayAssembler = new TItem[--Count];
+                    Array.Copy(m_array, arrayAssembler, i);
+                    Array.Copy(m_array, i + 1, arrayAssembler, i, Count - i);
+                    m_array = arrayAssembler;
+                    return;
+                }
+        }
+    }
+
     public void RemoveAt(int index)
     {
-            TItem[] arrayAssembler = new TItem[--Count];
-            Array.Copy(m_array, arrayAssembler, index);
-            Array.Copy(m_array, index + 1, arrayAssembler, index, Count - index);
-            m_array = arrayAssembler;
+        if(index < 0 || index > Count)
+        {
+            Console.WriteLine("Индекс за границами промежутка");
             return;
+        }
+        TItem[] arrayAssembler = new TItem[--Count];
+        Array.Copy(m_array, arrayAssembler, index);
+        Array.Copy(m_array, index + 1, arrayAssembler, index, Count - index);
+        m_array = arrayAssembler;
+        return;
     }
 
     public void Clear()
@@ -79,14 +146,31 @@ public class MyList<TItem> : IEnumerable<TItem>
             m_array = new TItem[Count];
     }
 
-    public void Insert(Int32 index, TItem item)
+    public bool Contains(TItem item)
     {
-            TItem[] arrayAssembler = new TItem[++Count];
-            Array.Copy(m_array, arrayAssembler, index);
-            arrayAssembler[index] = item;
-            Array.Copy(m_array, index, arrayAssembler, index + 1, Count - index - 1);
-            m_array = arrayAssembler;
-            return;
+        for(int i = 0; i < Count; i++)
+        {
+            if(Comparer<TItem>.Default.Compare(m_array[i], item) == 0)
+                {
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    public void Sort()
+    {
+        TItem temp;
+        for(int i = 0; i < Count - 1; i++)
+            for(int j = i + 1; j < Count; j++)
+            {
+                if(Comparer<TItem>.Default.Compare(m_array[i], m_array[j]) >= 0 )
+                {
+                    temp = m_array[i];
+                    m_array[i] = m_array[j];
+                    m_array[j] = temp;
+                }
+            }
     }
 
     public IEnumerator<TItem> GetEnumerator()
--- _3_1/_3/Program.cs
+++ _3_1/_3/Program.cs
@@ -5,40 +5,47 @@ class Program
     {
         MyList<int> myList = new MyList<int>();
         MyList<int> myList2 = new MyList<int>();
-
-
         var time = new Stopwatch();
-        time.Start();
-        time.Stop();
-        Console.WriteLine(time.Elapsed);
+
 
         Random rnd = new Random();
-        for(int i = 0; i < 100000; i++)
+        for(int i = 0; i < 5; i++)
         {
             myList.Add(rnd.Next(100));
         }
 
-        foreach (int item in myList)
-        {
-            myList2.Add(item);
-        }
-
-
-        time.Start();
-        myList.Remove(10);
-        time.Stop();
-        Console.WriteLine(time.Elapsed);
 
         // time.Start();
-        // myList2.RemoveTestForLoop(10);
+
         // time.Stop();
         // Console.WriteLine(time.Elapsed);
 
-        // foreach (int item in myList)
-        // {
-        //     Console.WriteLine(item);
-        // }
+        foreach (int item in myList)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("____________________________");
+        time.Start();
+        myList.Sort();
 
+        foreach (int item in myList)
+        {
+            Console.WriteLine(item);
+        }
+        time.Stop();
+        Console.WriteLine(time.Elapsed);
+
+        Console.WriteLine("____________________________");
+
+        int[] testtt = {666, 666};
+        myList.InsertRange(myList.Count, testtt);
+        foreach (int item in myList)
+        {
+            Console.WriteLine(item);
+        }
+        time.Stop();
+        Console.WriteLine(time.Elapsed);
+        
         Console.ReadKey();
     }
 }
